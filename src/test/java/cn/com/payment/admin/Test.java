104548b4073bd19ffa7186c77f6ae141cfa61258
duanli <44545328@qq.com>
2019-03-22 20:59:34 +0800

    branch test one

--- src/test/java/cn/com/payment/admin/Test.java
+++ src/test/java/cn/com/payment/admin/Test.java
@@ -5,5 +5,6 @@ import cn.com.payment.admin.utils.AmtUtils;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(Long.valueOf(AmtUtils.yuanToFen("10.00")));
+		System.out.println(Long.valueOf(AmtUtils.yuanToFen("10.00")));
 	}
 }
